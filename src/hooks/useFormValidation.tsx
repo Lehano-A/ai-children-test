13b8c4ed5e34d2d08437bdd6093286cc1ca1a031
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-06-08 11:25:44 +0300

    Добавляет хук валидации формы useFormValidation

---
+++ src/hooks/useFormValidation.tsx
@@ -0,0 +1,21 @@
+import { type RefObject } from 'react'
+import { useDispatch } from 'react-redux'
+import { validateForm } from '../redux/reducers/actions/validateForm.action'
+import type { Forms } from '../redux/reducers/slices/ui.slice'
+
+function useFormValidation(formRef: RefObject<HTMLFormElement>, formName: Forms) {
+  const dispatch = useDispatch()
+
+  return () => {
+    if (formRef.current) {
+      dispatch(
+        validateForm({
+          formName,
+          formRef: formRef as RefObject<HTMLFormElement>,
+        }),
+      )
+    }
+  }
+}
+
+export default useFormValidation
