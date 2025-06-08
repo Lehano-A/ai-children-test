68f63ea65d6c5af8b8edaa9e15f4f6c32e347469
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-06-08 11:26:34 +0300

    Добавляет компонент StepProgress

---
+++ src/components/StepProgress/StepProgress.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+interface ProgressBoxProps {
+  $totalParts: number
+}
+
+const StepProgressBox = styled('div')<ProgressBoxProps>`
+  display: grid;
+  grid-template-columns: ${({ $totalParts }) => `repeat(${$totalParts}, 1fr)`};
+  height: 16px;
+  margin: 0 -64px 48px;
+  background-color: ${({ theme }) => theme.palette.blue['50']};
+`
+
+const Part = styled('div')<{ $needHighlight: boolean }>`
+  background-color: ${({ $needHighlight, theme }) =>
+    $needHighlight ? theme.palette.blue['100'] : 'transparent'};
+`
+
+interface ProgressProps {
+  totalParts: number
+  step: number
+}
+
+function StepProgress({ totalParts, step }: ProgressProps) {
+  return (
+    <StepProgressBox $totalParts={totalParts}>
+      {new Array(totalParts).fill(0).map((_, id) => (
+        <Part key={id} $needHighlight={id + 1 <= step} />
+      ))}
+    </StepProgressBox>
+  )
+}
+
+export default StepProgress
