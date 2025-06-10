70eba8b80358d52d5d4b351ebe4590e3ae32e927
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-06-10 19:28:30 +0300

    Добавляет компонент индикатор загрузки Loader

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,30 @@
+import { CircularProgress } from '@mui/material'
+
+import { useTheme } from 'styled-components'
+import type { ThemeType } from '../../types/theme'
+
+function Loader({
+  color = 'surface1',
+  shade,
+}: {
+  color?: keyof ThemeType['palette']
+  shade?: number
+}) {
+  const theme = useTheme()
+
+  return (
+    <CircularProgress
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+        '& .MuiCircularProgress-circle': {
+          stroke: shade ? theme.palette[color][shade] : theme.palette[color],
+        },
+      }}
+    />
+  )
+}
+
+export default Loader
