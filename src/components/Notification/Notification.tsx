c703383bdfea89f13996b9f5f9167171515e04bc
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-06-10 21:48:10 +0300

    Добавляет компонент уведомлений Notification

---
+++ src/components/Notification/Notification.tsx
@@ -0,0 +1,37 @@
+import { Slide, Snackbar, type SlideProps } from '@mui/material'
+import { useDispatch, useSelector } from 'react-redux'
+import { useTheme } from 'styled-components'
+import { closeNotification } from '../../redux/reducers/slices/notification.slice'
+import type { RootState } from '../../redux/store'
+
+function SlideTransition(props: SlideProps) {
+  return <Slide {...props} direction='down' />
+}
+
+function Notification() {
+  const { isOpen, message, status } = useSelector((state: RootState) => state.notification)
+
+  const dispatch = useDispatch()
+
+  const theme = useTheme()
+
+  return (
+    <Snackbar
+      color='lime'
+      open={isOpen}
+      onClose={() => dispatch(closeNotification())}
+      slots={{ transition: SlideTransition }}
+      message={message}
+      key={Math.random()}
+      autoHideDuration={8000}
+      anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+      sx={{
+        '.MuiSnackbarContent-message': { display: 'flex', flexDirection: 'column' },
+
+        '.MuiPaper-root': { fontFamily: 'inherit', backgroundColor: theme.palette[status] },
+      }}
+    />
+  )
+}
+
+export default Notification
