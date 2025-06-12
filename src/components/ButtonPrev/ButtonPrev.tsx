cb322b82428e53a19a0bfd5b06dfadb5936a88dd
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-06-12 17:17:22 +0300

    Добавляет компонент ButtonPrev

---
+++ src/components/ButtonPrev/ButtonPrev.tsx
@@ -0,0 +1,24 @@
+import Button from '../common/Button/Button'
+import ArrowLeftIcon from '../../assets/icons/arrow-left.svg?react'
+import { useDispatch } from 'react-redux'
+import { prevStep } from '../../redux/reducers/slices/form.slice'
+
+function ButtonPrev() {
+  const dispatch = useDispatch()
+
+  function handleClick() {
+    dispatch(prevStep())
+  }
+
+  return (
+    <Button
+      buttonName='К загрузке рисунков'
+      icon={<ArrowLeftIcon />}
+      variant='secondary'
+      iconPosition='left'
+      handleClick={handleClick}
+    />
+  )
+}
+
+export default ButtonPrev
