15d3750c3cab12e654e89782c82e020c49552b83
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-06-14 20:53:55 +0300

    Добавляет api

---
+++ src/api/api.ts
@@ -0,0 +1,48 @@
+import type { Survey } from '../components/sections/SurveySection/survey.types'
+
+class Api {
+  baseUrl: string
+
+  constructor() {
+    this.baseUrl = 'https://sirius-draw-test-94500a1b4a2f.herokuapp.com'
+  }
+
+  uploadImages = (formData: FormData) => {
+    return fetch(`${this.baseUrl}/upload`, {
+      method: 'POST',
+      body: formData,
+    }).then((res) => {
+      return this._getResponse(res)
+    })
+  }
+
+  sendSurvey = (data: Survey) => {
+    return fetch(`${this.baseUrl}/submit-survey`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/JSON' },
+
+      body: JSON.stringify(data),
+    }).then((res) => {
+      return this._getResponse(res)
+    })
+  }
+
+  getStatusResultTest = (task_id: string) => {
+    return fetch(`${this.baseUrl}/report/${task_id}`).then((res) => {
+      return this._getResponse(res)
+    })
+  }
+
+  async _getResponse(res: Response) {
+    if (!res.ok) {
+      const message = await res.json()
+
+      throw new Error(message.detail)
+    }
+
+    return res.json()
+  }
+}
+const api = new Api()
+
+export default api
