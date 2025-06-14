a3a56e410069c819bbb3f9a605adaae402c3a286
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-06-14 20:56:08 +0300

    Добавляет конфиги приложения, а также contstants, index.tsx и index.html

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const IMAGE_UPLOAD_FORM = 'imageUploadForm'
+export const SURVEY_FORM = 'surveyForm'
+export const RESULT_PROCESSING = 'resultProcessing'
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Normalize } from 'styled-normalize'
-
-import App from './App.tsx'
+import './assets/fonts/fonts.css'
+import App from './components/App/App.tsx'
+import { ThemeProvider } from 'styled-components'
+import lightTheme from './theme.ts'
+import { GlobalStyles } from './styled/globalStyles.ts'
+import store from './redux/store'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Normalize />
-    <App />
+    <Provider store={store}>
+      <ThemeProvider theme={lightTheme}>
+        <Normalize />
+        <GlobalStyles />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </StrictMode>,
 )
