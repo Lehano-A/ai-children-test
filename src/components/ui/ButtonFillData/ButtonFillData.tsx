ed1a1c3f3105887bffd67ccf9032a823a139ea25
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-06-24 16:24:54 +0300

    Выносит кнопку ButtonFillData в отдельный компонент

---
+++ src/components/ui/ButtonFillData/ButtonFillData.tsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components'
+import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome'
+
+const Button = styled('button')`
+  position: fixed;
+  z-index: 100000;
+  bottom: 20px;
+  right: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 35px;
+  height: 35px;
+  border-radius: 50%;
+  border: none;
+  background-color: ${({ theme }) => theme.palette.blue['100']};
+  padding: 8px;
+  cursor: pointer;
+  transition: background-color 0.15s ease;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.palette.blue['110']};
+  }
+
+  & svg {
+    width: 100%;
+    height: 100%;
+    fill: ${({ theme }) => theme.palette.surface1};
+  }
+`
+
+function ButtonFillData({ handleFill }: { handleFill: () => void }) {
+  return (
+    <Button onClick={handleFill}>
+      <AutoAwesomeIcon />
+    </Button>
+  )
+}
+
+export default ButtonFillData
